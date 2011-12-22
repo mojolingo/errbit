58ab94e727e4b30bee128db52c0bbfc4af5ad5f2
Ben Klang <bklang@mojolingo.com>
2011-12-22 16:37:55 -0500

    Correct detection/storage of name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,9 +38,10 @@ class User
 
   before_save :ensure_authentication_token
 
-  before_save :set_ldap_email
-  def set_ldap_email
-    self.email = Devise::LdapAdapter.get_ldap_param(self.username, "mail")
+  before_save :set_ldap_info
+  def set_ldap_info
+    self.email = Devise::LdapAdapter.get_ldap_param(self.username, "mail").first
+    self.name = Devise::LdapAdapter.get_ldap_param(self.username, "cn").first
   end
 
   validates_presence_of :name
