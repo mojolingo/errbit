1c76937d18477ca577761d1a1f1ddfa02df29fd0
Ben Klang <bklang@mojolingo.com>
2011-12-22 16:38:06 -0500

    Fix creating users on first login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,12 +8,12 @@ Devise.setup do |config|
 
   # ==> LDAP Configuration
   config.ldap_logger = true
-  config.ldap_create_user = false
+  config.ldap_create_user = true
   # config.ldap_update_password = true
   config.ldap_config = "#{Rails.root}/config/ldap.yml"
   config.ldap_check_group_membership = false
   config.ldap_check_attributes = false
-  config.ldap_use_admin_to_bind = false
+  config.ldap_use_admin_to_bind = true
   # config.ldap_ad_group_check = false
   config.ldap_auth_username_builder = Proc.new do |attribute, login, ldap|
     login.gsub!(/@.*$/, '')
